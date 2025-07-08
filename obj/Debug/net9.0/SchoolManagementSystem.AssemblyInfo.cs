84fd1f78581274e667b0cca0869c5e5909f8fa50
Alqan Nazra <alqan.nazra02@gmail.com>
2025-07-08 09:37:38 +0700

    Update assembly informational version and cache inputs

--- obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d5f420a388a6b71ff2327dfec6fe5ec5c9d810")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d385b9093fa143141153d8c0141978797da469")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
